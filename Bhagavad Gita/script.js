d94e747e88eb1dca6aefc7bb56cb7aaf4d88e0b8
Shivam Saxena <91417928+shiv-am-saxenaa@users.noreply.github.com>
2023-05-07 13:07:12 +0530

    changes

--- Bhagavad Gita/script.js
+++ Bhagavad Gita/script.js
@@ -15,11 +15,25 @@ try {
         ihtml = ""
 		for(items in res)
 		{
-			console.log(res[items])
-
+			console.log(res[items]);
+			ihtml += 
+			`<div class="card col-12 col-md-6 col-lg-4 col-xl-3 m-5 p-2">
+				<img
+					src="https://sourabhmore.com/wp-content/uploads/2021/04/Bhagwat-Gita.jpg"
+					class="card-img-top bg-dark"
+					alt="..." />
+				<div class="card-body">
+					<h5 class="card-title">${res[items].name}</h5>
+					<p class="card-text">
+						Some quick example text to build on the card title
+						and make up the bulk of the card's content.
+					</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>`
+			document.getElementsByClassName("rows")[0].innerHTML = ihtml;
 		}
 	})
 } catch (error) {
 	console.error(error);
 }
-// document.getElementsByClassName("navbar-brand")[0]
