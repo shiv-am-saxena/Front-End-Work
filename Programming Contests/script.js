59bf94eae5ae25301569371bf91ed3c7e28a0c08
Shivam Saxena <91417928+shiv-am-saxenaa@users.noreply.github.com>
2023-05-08 18:37:43 +0530

    Programming Contest Done

---
+++ Programming Contests/script.js
@@ -0,0 +1,29 @@
+let url = "https://kontests.net/api/v1/all";
+try {
+	const  response = fetch(url);
+	response.then((v)=>{
+        return v.json()
+    }).then((res)=>{
+        ihtml = ""
+		for(items in res)
+		{
+			ihtml += `
+            <div class="card col-12 col-md-6 col-lg-4 col-xl-3 m-5 p-2">
+                <h1 class="card-img-top bg-light">${res[items].site}</h1>
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title"><strong>${res[items].name}</strong></h5>
+                    <p class="card-text">
+                        <strong>Start-time</strong>: ${res[items].start_time}<br>
+                        <strong>End-time</strong>: ${res[items].end_time}<br>
+                        <strong>Duration</strong>: ${res[items].duration}
+                    </p>
+                    <a href="${res[items].url}" class="btn mt-auto btn-primary" target="_blank">Go to contest</a>
+                </div>
+            </div>
+            `
+			document.getElementsByClassName("row")[0].innerHTML = ihtml;
+		}
+	})
+} catch (error) {
+	console.error(error);
+}
