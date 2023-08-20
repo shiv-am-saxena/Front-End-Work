aad7433f0336a41646a0dc8b804f10db99dd139f
Shivam Saxena <shiv-am-saxena@users.noreply.github.com>
2023-08-20 14:34:50 +0530

    BubbleGameJS

---
+++ Bubble Game JS/script.js
@@ -0,0 +1,46 @@
+
+let hitnum = 0;
+let timerVal = 60;
+let score = 0;
+function newHit() {
+    hitnum = Math.floor(Math.random() * 10);
+    document.querySelector("#hitid").textContent = hitnum;
+}
+function bubbleMaker() {
+    let bubble = "";
+    for (let i = 0; i <= 215; i++) {
+        bubble += `<div class="bubble">${Math.floor(Math.random() * 10)}</div>`;
+    }
+    document.querySelector("#pbtm").innerHTML = bubble;
+}
+function timerClock() {
+    let timer = setInterval(function () {
+        if (timerVal > 0) {
+            timerVal--;
+            document.querySelector("#time").textContent = timerVal;
+        }
+        else{
+            clearInterval(timer);
+            document.querySelector("#pbtm").innerHTML = `<h1>Game Over</h1>`;
+            document.querySelector("#pbtm").style.color = "Green";
+        }
+    }, 1000);
+}
+function scoreIncrement()
+{
+    score+=10;
+    document.querySelector("#scoreVal").textContent = score;
+}
+document.querySelector("#pbtm").addEventListener("click", function(details){
+    let num = Number(details.target.textContent);
+    // console.log(num , hitnum);
+    if (num === hitnum)
+    {
+        bubbleMaker();
+        scoreIncrement();
+        newHit();
+    }
+})
+bubbleMaker();
+timerClock();
+newHit();
