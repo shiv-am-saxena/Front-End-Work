570daf23d82a01d6ca0196319e5a90bbf88f1ffd
Shivam Saxena <shivam2003official@gmail.com>
2024-07-18 20:25:34 +0530

    Update script.js

--- Bhagavad Gita/script.js
+++ Bhagavad Gita/script.js
@@ -1,76 +1,74 @@
 const url = 'https://bhagavad-gita3.p.rapidapi.com/v2/chapters/';
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'fc67eab140mshdbdff4fa926cd48p190615jsneca443d030f1',
-		'X-RapidAPI-Host': 'bhagavad-gita3.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'fc67eab140mshdbdff4fa926cd48p190615jsneca443d030f1',
+        'X-RapidAPI-Host': 'bhagavad-gita3.p.rapidapi.com'
+    }
 };
 
-try {
-	const  response = fetch(url, options);
-	response.then((v)=>{
-        return v.json()
-    }).then((res)=>{
-        ihtml = ""
-		for(items in res)
-		{
-			ihtml += 
-			`<div class="card col-12 col-md-6 col-lg-4 col-xl-3 m-5 p-2">
-				<img
-					src="https://wallpaperaccess.com/full/4524218.jpg"
-					class="card-img-top bg-dark"
-					alt="..." />
-				<div class="card-body d-flex flex-column">
-					<h5 class="card-title"><strong>नाम </strong>: ${res[items].name}</h5>
-					<p class="card-text">
-						<strong>सारांश</strong>: ${res[items].chapter_summary_hindi}
-					</p>
-					<button type="button" class="btn mt-auto btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop${res[items].chapter_number}">
-						<strong>श्लोक पढ़ें</strong>
-					</button>
-				</div>
-			</div>
 
-			<!-- Modal -->
-			<div class="modal fade" id="staticBackdrop${res[items].chapter_number}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-			<div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
-				<div class="modal-content">
-				<div class="modal-header">
-					<h1 class="modal-title fs-5" id="staticBackdropLabel">${res[items].name}</h1>
-					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-				</div>
-				<div class="modal-body">
-					<div class="accordion" id="accordionExample">
+
+
+const fetchChapters = async () => {
+    try {
+        const response = await fetch(url, options);
+        const res = await response.json();
+        let ihtml = "";
+        for (items in res) {
+            ihtml +=
+                `<div class="card col-12 col-md-6 col-lg-4 col-xl-3 m-5 p-2">
+					<img
+						src="https://wallpaperaccess.com/full/4524218.jpg"
+						class="card-img-top bg-dark"
+						alt="..." />
+					<div class="card-body d-flex flex-column">
+						<h5 class="card-title"><strong>नाम </strong>: ${res[items].name}</h5>
+						<p class="card-text">
+							<strong>सारांश</strong>: ${res[items].chapter_summary_hindi}
+						</p>
+						<button type="button" class="btn mt-auto btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop${res[items].chapter_number}">
+							<strong>श्लोक पढ़ें</strong>
+						</button>
 					</div>
 				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+	
+				<!-- Modal -->
+				<div class="modal fade" id="staticBackdrop${res[items].chapter_number}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+				<div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+					<div class="modal-content">
+					<div class="modal-header">
+						<h1 class="modal-title fs-5" id="staticBackdropLabel">${res[items].name}</h1>
+						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+					</div>
+					<div class="modal-body">
+						<div class="accordion" id="accordionExample">
+						</div>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+					</div>
+					</div>
 				</div>
-				</div>
-			</div>
-			</div>`
-			document.getElementsByClassName("row")[0].innerHTML = ihtml;
-		}
-	})
-} catch (error) {
-	console.error(error);
+				</div>`
+            document.getElementsByClassName("row")[0].innerHTML = ihtml;
+        }
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-try
-{
-	for(let i = 1;i<=18;i++)
-	{
-		const link = `https://bhagavad-gita3.p.rapidapi.com/v2/chapters/${i}/verses/`;
-		try {
-			const value = fetch(link, options);
-			value.then((gita)=>{
-				return gita.json();
-			}).then((result)=>{
-				ihtm = ""
-				for(item in result)
-				{
-					ihtm += `
+
+const fetchVerses = async () => {
+    try {
+        for (let i = 1; i <= 18; i++) {
+            const link = `https://bhagavad-gita3.p.rapidapi.com/v2/chapters/${i}/verses/`;
+            try {
+                const value = await fetch(link, options);
+                const result = await value.json();
+                let ihtm = "";
+                for (item in result) {
+                    ihtm += `
 					<div class="accordion-item">
 						<h2 class="accordion-header" id="headingOne">
 						<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${result[item].chapter_number}-${result[item].verse_number}" aria-expanded="true" aria-controls="collapseOne">
@@ -89,13 +87,16 @@ try
 						</div>
 						</div>
 					</div>`
-					document.getElementsByClassName("accordion")[i-1].innerHTML = ihtm;
-				}
-			})
-		} catch (error) {
-			console.error(error);
-		}
-	}
-} catch(error){
-	console.error(error)
+                    document.getElementsByClassName("accordion")[i - 1].innerHTML = ihtm;
+                }
+            } catch (error) {
+                console.error(error.message);
+            }
+        }
+    } catch (error) {
+        console.error(error.message)
+    }
 }
+
+fetchChapters()
+fetchVerses()
